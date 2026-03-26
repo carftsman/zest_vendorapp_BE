4513f67717e1d22c6e4d3f08100dde847be398e7
Naveendbs137 <venkatanaveen.t@dhatvibs.com>
2026-03-26 11:45:09 +0530

    Project Folder Structure

---
+++ Zest_Vendorapp_Onboarding_Service/src/main/java/com/dhatvibs/ZestVendorappOnboardingApplication.java
@@ -0,0 +1,19 @@
+package com.dhatvibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication  //(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableFeignClients
+
+public class ZestVendorappOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZestVendorappOnboardingApplication.class, args);
+	}
+
+}
---
+++ zest_vendorapp_Registry/src/main/java/com/dhatvibs/ZestVendorappRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.dhatvibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //added for enable eureka server
+public class ZestVendorappRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZestVendorappRegistryApplication.class, args);
+	}
+
+}
